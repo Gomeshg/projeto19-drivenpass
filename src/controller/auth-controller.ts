26a58eb092bab6a0ab9dddf5407c66a0c01a9b79
Hugo Gomes <gomes.hugo00@gmail.com>
2022-12-10 11:21:07 -0300

    refactor: create auth router and controllers scope

---
+++ src/controller/auth-controller.ts
@@ -0,0 +1,7 @@
+import { Request, Response } from "express";
+
+async function signUp(req: Request, res: Response) {}
+
+async function signIn(req: Request, res: Response) {}
+
+export { signUp, signIn };
