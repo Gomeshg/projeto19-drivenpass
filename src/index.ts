ea0f0d9254b895e8f9c5c5f80681cf14dc2759cc
Hugo Gomes <gomes.hugo00@gmail.com>
2022-12-10 11:28:22 -0300

    feat: prisma connected

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
+import prisma from "./database/database.js";
 
-dotenv.config()
+dotenv.config();
 
 const server = express();
 
 server.listen(process.env.PORT, () => {
-    console.log(`Servidor rodando na porta ${process.env.PORT}`);
+  console.log(`Servidor rodando na porta ${process.env.PORT}`);
+  if (prisma) {
+    console.log("Banco de dados conectado com sucesso!");
+  }
 });
